5d9796bb03d8472259cae3972adf6877488be133
ogabek <norpulatovogabek42@gmail.com>
2022-07-12 22:24:02 +0500

    dfgfdg

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -4,6 +4,31 @@
     Args:
         list1 (list): parameter
     Returns:
-        bool: return answer
+        bool: return answe7
 */
-void main() {}
+bool func(list1) {
+  int q = 0;
+  bool w = true;
+  bool p = false;
+  while (q > list1.length) {
+    q++;
+    if (list1[q] == 0 ||
+        list1[q] == 1 ||
+        list1[q] == 2 ||
+        list1[q] == 3 ||
+        list1[q] == 4 ||
+        list1[q] == 5 ||
+        list1[q] == 6 ||
+        list1[q] == 7 ||
+        list1[q] == 8 ||
+        list1[q] == 9) {
+      return true;
+    } else
+      return false;
+  }
+  return false;
+}
+
+void main() {
+  print(func([4, 4, 4, 4]));
+}
