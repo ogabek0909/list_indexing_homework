e62f839316845d3dae1b17c715d45de40f411fba
ogabek <norpulatovogabek42@gmail.com>
2022-07-12 21:38:55 +0500

    sdf

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -6,4 +6,20 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int x = -1;
+  bool p = true;
+  bool w = false;
+  while (x < list1.length - 1) {
+    x++;
+    if (list1[x] == 1) {
+      list1[x] = p;
+    } else
+      list1[x] = w;
+  }
+  return list1;
+}
+
+void main() {
+  print(func([0, 1, 1, 0, 1]));
+}
