296be74e140ef9b511b91765d73e16608b466879
ogabek <norpulatovogabek42@gmail.com>
2022-07-12 17:54:42 +0500

    a

--- bin/list_index06.dart
+++ bin/list_index06.dart
@@ -1,9 +1,25 @@
 /*
     Create function named func with arguments list1
-    A list of units and zeros with a length of five is given. Replace one with True.
+    A list of units and zeros with a  given. Replace one with True.
     Args:
         list1 (list): parameter
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int q = 0;
+
+  bool z = true;
+  while (q <= list1.length - 1) {
+    q++;
+    if (list1[q] == 1) {
+      list1[q] = z;
+    }
+  }
+
+  return list1;
+}
+
+void main() {
+  print(func([1, 0, 1, 0, 1]));
+}
